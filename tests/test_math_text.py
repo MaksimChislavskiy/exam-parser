c9d5693c93e980338708becdc963f4a29eb8407a
Максим <maxwell884884@gmail.com>
2026-07-22 21:55:23 +0300

    Add geometry notation regression tests

--- tests/test_math_text.py
+++ tests/test_math_text.py
@@ -49,8 +49,75 @@ class LatexDelimiterTests(unittest.TestCase):
 
         self.assertEqual(
             task.condition,
-            "В кубе $ABCDA_1B_1C_1D_1$ дана точка K.",
+            "В кубе $ABCDA_1B_1C_1D_1$ дана точка $K$.",
         )
+
+
+class GeometryNotationTests(unittest.TestCase):
+    def test_wraps_plain_latin_geometry_labels(self) -> None:
+        task = ExtractedTask(
+            task_num="3",
+            condition="В треугольнике ABC стороны AB и BC равны.",
+        )
+
+        self.assertEqual(
+            task.condition,
+            "В треугольнике $ABC$ стороны $AB$ и $BC$ равны.",
+        )
+
+    def test_converts_cyrillic_confusables_and_unicode_indices(self) -> None:
+        task = ExtractedTask(
+            task_num="3",
+            condition=(
+                "Призма АВСА₁В₁С₁, точка М и пирамида ВСС₁М."
+            ),
+        )
+
+        self.assertEqual(
+            task.condition,
+            "Призма $ABCA_1B_1C_1$, точка $M$ и пирамида $BCC_1M$.",
+        )
+
+    def test_merges_subscript_split_into_separate_math_span(self) -> None:
+        task = ExtractedTask(
+            task_num="14",
+            condition="Между прямыми МС и ВС $ _{1} $ выбрана точка М.",
+        )
+
+        self.assertEqual(
+            task.condition,
+            "Между прямыми $MC$ и $BC_1$ выбрана точка $M$.",
+        )
+
+    def test_preserves_existing_math_while_merging_ocr_subscript(self) -> None:
+        task = ExtractedTask(
+            task_num="14",
+            condition="Прямые $MC$ и $BC$ $ _{1} $ различны.",
+        )
+
+        self.assertEqual(
+            task.condition,
+            "Прямые $MC$ и $BC$ $ _{1} $ различны.",
+        )
+
+    def test_preserves_non_geometry_uppercase_abbreviations(self) -> None:
+        task = ExtractedTask(
+            task_num="19",
+            condition="Найдите НОД и НОК данных чисел.",
+        )
+
+        self.assertEqual(
+            task.condition,
+            "Найдите НОД и НОК данных чисел.",
+        )
+
+    def test_plain_code_without_geometry_context_is_not_wrapped(self) -> None:
+        task = ExtractedTask(
+            task_num="1",
+            condition="Введите код ABC.",
+        )
+
+        self.assertEqual(task.condition, "Введите код ABC.")
 
 
 if __name__ == "__main__":
