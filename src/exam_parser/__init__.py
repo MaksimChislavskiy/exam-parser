123979d71ebad2931be8cd2b78e9064fd358341d
Максим <maxwell884884@gmail.com>
2026-08-06 19:54:41 +0300

    Install universal condition repairs

--- src/exam_parser/__init__.py
+++ src/exam_parser/__init__.py
@@ -1,5 +1,9 @@
 """Извлечение математических задач из распознанных страниц."""
 
 from .models import ExtractedTask, TaskRecord, TaskSolution
+from .condition_repairs import install_condition_repairs
+
+install_condition_repairs()
+del install_condition_repairs
 
 __all__ = ["ExtractedTask", "TaskRecord", "TaskSolution"]
