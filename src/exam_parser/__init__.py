33c9e567b5905c7fc7de9854cae8164bd4471c55
Максим <maxwell884884@gmail.com>
2026-08-06 20:31:49 +0300

    Install universal OCR and boundary repairs

--- src/exam_parser/__init__.py
+++ src/exam_parser/__init__.py
@@ -2,8 +2,18 @@
 
 from .models import ExtractedTask, TaskRecord, TaskSolution
 from .condition_repairs import install_condition_repairs
+from .ocr_context_repairs import install_ocr_context_repairs
+from .source_repairs import install_source_repairs
+from .boundary_repairs import install_boundary_repairs
 
 install_condition_repairs()
+install_ocr_context_repairs()
+install_source_repairs()
+install_boundary_repairs()
+
 del install_condition_repairs
+del install_ocr_context_repairs
+del install_source_repairs
+del install_boundary_repairs
 
 __all__ = ["ExtractedTask", "TaskRecord", "TaskSolution"]
