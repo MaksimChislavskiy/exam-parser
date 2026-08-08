f8066d4526af8291afb1f28ff39b494d0c53b34d
Максим <maxwell884884@gmail.com>
2026-08-08 16:49:25 +0300

    Install universal image repairs

--- src/exam_parser/__init__.py
+++ src/exam_parser/__init__.py
@@ -5,15 +5,18 @@ from .condition_repairs import install_condition_repairs
 from .ocr_context_repairs import install_ocr_context_repairs
 from .source_repairs import install_source_repairs
 from .boundary_repairs import install_boundary_repairs
+from .image_repairs import install_image_repairs
 
 install_condition_repairs()
 install_ocr_context_repairs()
 install_source_repairs()
 install_boundary_repairs()
+install_image_repairs()
 
 del install_condition_repairs
 del install_ocr_context_repairs
 del install_source_repairs
 del install_boundary_repairs
+del install_image_repairs
 
 __all__ = ["ExtractedTask", "TaskRecord", "TaskSolution"]
