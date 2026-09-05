ab3a9e62ea7e5eef808939aa70b97f1f9eb3f12b
Максим <maxwell884884@gmail.com>
2026-09-05 16:20:36 +0300

    Enable terminal PDF reference recovery

--- src/exam_parser/__init__.py
+++ src/exam_parser/__init__.py
@@ -8,6 +8,7 @@ from .boundary_repairs import install_boundary_repairs
 from .image_repairs import install_image_repairs
 from .release_quality_repairs import install_release_quality_repairs
 from .release_quality_repairs_v2 import install_release_quality_repairs_v2
+from .pdf_reference_repairs import install_pdf_reference_repairs
 
 install_condition_repairs()
 install_ocr_context_repairs()
@@ -16,6 +17,7 @@ install_boundary_repairs()
 install_image_repairs()
 install_release_quality_repairs()
 install_release_quality_repairs_v2()
+install_pdf_reference_repairs()
 
 del install_condition_repairs
 del install_ocr_context_repairs
@@ -24,5 +26,6 @@ del install_boundary_repairs
 del install_image_repairs
 del install_release_quality_repairs
 del install_release_quality_repairs_v2
+del install_pdf_reference_repairs
 
 __all__ = ["ExtractedTask", "TaskRecord", "TaskSolution"]
