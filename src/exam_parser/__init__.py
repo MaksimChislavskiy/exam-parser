e4063b6373e43ade8a89e83b72d397c9b6f72726
Максим <maxwell884884@gmail.com>
2026-09-05 15:19:57 +0300

    Enable final release quality repairs

--- src/exam_parser/__init__.py
+++ src/exam_parser/__init__.py
@@ -6,17 +6,20 @@ from .ocr_context_repairs import install_ocr_context_repairs
 from .source_repairs import install_source_repairs
 from .boundary_repairs import install_boundary_repairs
 from .image_repairs import install_image_repairs
+from .release_quality_repairs import install_release_quality_repairs
 
 install_condition_repairs()
 install_ocr_context_repairs()
 install_source_repairs()
 install_boundary_repairs()
 install_image_repairs()
+install_release_quality_repairs()
 
 del install_condition_repairs
 del install_ocr_context_repairs
 del install_source_repairs
 del install_boundary_repairs
 del install_image_repairs
+del install_release_quality_repairs
 
 __all__ = ["ExtractedTask", "TaskRecord", "TaskSolution"]
